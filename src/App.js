6202b03804872bda9f89a6986a5bad76db58d733
Camil Plojovic <camil.plojovic@levi9.com>
2022-08-04 18:49:58 +0200

    Created TIC_TAC_TOE game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const winningCombinations = [
+  ["00", "01", "02"],
+  ["10", "11", "12"],
+  ["20", "21", "22"],
+  ["00", "10", "20"],
+  ["01", "11", "21"],
+  ["02", "12", "22"],
+  ["00", "11", "22"],
+  ["02", "11", "20"],
+];
+
+const Game = () => {
+  const [XO, setXO] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
+  const [isFirstPlayer, setIsFirstPlayer] = useState(true);
+  const [winningPlayer, setWinningPlayer] = useState("");
+
+  const handleOnClick = (element) => {
+    let xoCopy = [...XO];
+    // let xoCopy = [      <--- isto ko linija iznad
+    //               ["X", null, "O"],
+    //               [null, "X", null],
+    //               [null, "X", null]]
+
+    // if (xoCopy[element.id[0]][element.id[1]] === null)
+    if (winningPlayer === "") {
+      if (!xoCopy[element.id[0]][element.id[1]]) {
+        if (isFirstPlayer) {
+          xoCopy[element.id[0]][element.id[1]] = "X";
+        } else {
+          xoCopy[element.id[0]][element.id[1]] = "O";
+        }
+
+        setXO(xoCopy);
+        setIsFirstPlayer(!isFirstPlayer);
+      }
+    }
+
+    const winner = whoWon();
+
+    if (winner !== null) {
+      setWinningPlayer(winner + " is a winner");
+    }
+  };
+
+  const whoWon = () => {
+    let winningPlayer = null;
+
+    winningCombinations.forEach((wc) => {
+      let combination = [];
+      wc.forEach((sqr) => {
+        combination.push(XO[sqr[0]][sqr[1]]);
+      });
+
+      if (
+        combination[0] === combination[1] &&
+        combination[0] === combination[2]
+      ) {
+        winningPlayer = combination[0];
+      }
+    });
+    return winningPlayer;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{winningPlayer}</h1>
+      {XO.map((row, i) => {
+        return (
+          <div key={row.toString() + i.toString()} className="row">
+            {row.map((square, j) => {
+              return (
+                <div
+                  key={square + j.toString()}
+                  id={i.toString() + j.toString()}
+                  onClick={(el) => handleOnClick(el.target)}
+                  className="square"
+                >
+                  {square}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
-  );
-}
 
-export default App;
+    // <div>
+    //   <div className="row">
+    //     <div className="square"></div>
+    //     <div className="square"></div>
+    //     <div className="square"></div>
+    //   </div>
+    //   <div className="row">
+    //     <div className="square"></div>
+    //     <div className="square"></div>
+    //     <div className="square"></div>
+    //   </div>
+    //   <div className="row">
+    //     <div className="square"></div>
+    //     <div className="square"></div>
+    //     <div className="square"></div>
+    //   </div>
+    // </div>
+  );
+};
+
+export default Game;
